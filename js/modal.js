ff6a20ebd26570eb28de9f73a3ed2dab7a42f325
SerhiiMK <serkatsan@gmail.com>
2023-01-17 10:37:01 +0200

    ДЗ-7 Нова надія

--- js/modal.js
+++ js/modal.js
@@ -1,35 +1,14 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
-// })();
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-  menuBtnRef.addEventListener('click', () => {
-    menuBtnRef.classList.toggle('is-open');
-  });
-})();
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-(() => {
-  const menuContainer = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
-  const toggleMenu = () => menuContainer.classList.toggle('is-open');
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  window
-    .matchMedia('(min-width: 600px)')
-    .addEventListener('change', e => e.matches && menuContainer.classList.remove('is-open'));
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
